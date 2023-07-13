c25a3575b131479d86f2033a1c965a5103a51ccb
Niels Dossche <niels.dossche@ugent.be>
2023-07-14 01:46:34 +0200

    Pass options

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@
 /// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 /// option. This file may not be copied, modified, or distributed
 /// except according to those terms.
-
 pub mod handle;
 pub mod libxml2;
 pub mod sink;
@@ -17,14 +16,29 @@ use std::default::Default;
 use std::io;
 
 use crate::sink::Sink;
-use html5ever::parse_document;
+use html5ever::interface::QuirksMode;
 use html5ever::tendril::TendrilSink;
+use html5ever::tree_builder::TreeBuilderOpts;
+use html5ever::{parse_document, ParseOpts};
 
 fn main() {
     let sink = Sink::new();
     let stdin = io::stdin();
-    parse_document(sink, Default::default())
-        .from_utf8()
-        .read_from(&mut stdin.lock())
-        .unwrap();
+    parse_document(
+        sink,
+        ParseOpts {
+            tokenizer: Default::default(),
+            tree_builder: TreeBuilderOpts {
+                exact_errors: true,
+                scripting_enabled: false,
+                iframe_srcdoc: false,
+                drop_doctype: false,
+                ignore_missing_rules: false,
+                quirks_mode: QuirksMode::NoQuirks,
+            },
+        },
+    )
+    .from_utf8()
+    .read_from(&mut stdin.lock())
+    .unwrap();
 }
